7923f6410800b7047686eef5d42d47f84a5722f0
Fernando Coltri <fcoltri@gmail.com>
2021-09-20 15:39:31 -0300

    projeto criado

---
+++ src/main/java/br/com/treswxsolutions/ConsumindorfbwsApplication.java
@@ -0,0 +1,13 @@
+package br.com.treswxsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumindorfbwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumindorfbwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/treswxsolutions/ConsumindorfbwsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treswxsolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumindorfbwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
